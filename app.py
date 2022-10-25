e88923dd89ece7a5999600f82ba41b7b906eef01
sunridden <kevinszeto9449@gmail.com>
2022-10-25 16:54:25 -0700

    Added main script

---
+++ app.py
@@ -0,0 +1,49 @@
+import random
+
+activityList = [] 
+losers = []
+loadedActivities = False
+foundWinner = False
+
+print('Enter all activities below. Enter "stop" when finished.')
+
+#Stores all user inputted activities
+while(loadedActivities != True):
+    newActivity = input()
+    if (newActivity == 'stop' or newActivity == 'Stop'):
+        loadedActivities = True
+        continue
+    print('Please enter the next activity or "stop" to finish.')
+    activityList.append(newActivity)
+
+#Pairs the activites to 'fight' one another in a random draw until one activity remains
+
+while(foundWinner != True):
+    index = 0
+    startingIndex = 0
+    if (len(activityList) == 1 or len(activityList) == 0):
+        foundWinner = True
+        continue
+    if (len(activityList) % 2 != 0):
+        startingIndex = 1
+        index = 1 
+    for activity in range(startingIndex, len(activityList), 2): 
+        first = activityList[index]
+        second = activityList[index + 1]
+        print(first + ' vs ' + second);
+        roundWinner = random.randint(0, 1)
+        if (roundWinner == 0):
+            winnerIndex = index
+            loserIndex = index + 1
+        else:
+            winnerIndex = index + 1
+            loserIndex = index
+        print('Winner is ' + activityList[winnerIndex])
+        index += 2
+        losers.append(activityList[loserIndex])
+
+    for x in losers:
+        activityList.remove(x)
+    losers = []
+
+print(activityList)
