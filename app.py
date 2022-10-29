7dea6a969cc5872fc15c69b189402cfda79022a9
sunridden <kevinszeto9449@gmail.com>
2022-10-28 23:22:55 -0700

    Code readability

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import random
 
 #Stores all user inputted activities
-def addActivities(activityList):
+def addActivities():
+    activityList = []
     loadedActivities = False
     print('Enter all activities below. Enter "stop" when finished.')
+
     while(loadedActivities != True):
         newActivity = input()
         if (newActivity.lower() == 'stop'):
@@ -12,59 +14,57 @@ def addActivities(activityList):
         if (newActivity != '' and newActivity not in activityList):
             activityList.append(newActivity)
         print('Please enter the next activity or "stop" to finish.')
+    
+    findWinner(activityList)
 
 #Pairs the activites to 'fight' one another in a random draw until one activity remains
 def findWinner(activityList):
     foundWinner = False
     losers = []
     while(foundWinner != True):
-        index = 0
-        startingIndex = 0
+        startIndex = 0
 
         if (len(activityList) == 1 or len(activityList) == 0):
             foundWinner = True
             continue
         if (len(activityList) % 2 != 0):
-            startingIndex = 1
-            index = 1 
+            startIndex = 1 
             
-        for activity in range(startingIndex, len(activityList), 2): 
-            first = activityList[index]
-            second = activityList[index + 1]
+        for activityIndex in range(startIndex, len(activityList), 2): 
+            first = activityList[activityIndex]
+            second = activityList[activityIndex + 1]
             roundWinner = random.randint(0, 1)
 
             if (roundWinner == 0):
-                loserIndex = index + 1
+                loserIndex = startIndex + 1
                 print(first + ' (winner) vs ' + second);
             else:
-                loserIndex = index
+                loserIndex = startIndex
                 print(first + ' vs ' + second + ' (winner)');
 
-            index += 2
+            startIndex += 2
             losers.append(activityList[loserIndex])
 
-        for x in losers:
-            activityList.remove(x)
+        for loser in losers:
+            activityList.remove(loser)
         losers = []
 
     winner = ""
     winner = winner.join(activityList)
-
     print("Your chosen activity is " + winner)
+    activityList.remove(winner)
 
 def startProgram():
     programRunning = True
-    activityList = [] 
 
     while (programRunning == True):
         print('Type "0" to manually input activities or type "1" to stop the program')
         userInput = input()
-        if (userInput.lower() == 'finish' or userInput == '1'):
+        if (userInput.lower() == 'stop' or userInput == '1'):
             programRunning = False
             continue
         if (userInput == '0'):
-            addActivities(activityList)
-            findWinner(activityList)
+            addActivities()
         elif (userInput != 0 or userInput != 1):
             print('The only allowed inputs are "0" or "1" for manual input')
 
